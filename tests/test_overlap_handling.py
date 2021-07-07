59e3ab75288f21dbbd5bb35f512a2d13999ab57d
Louis Maddox <louismmx@gmail.com>
2021-07-07 17:26:10 +0100

    Begin a proper test

--- tests/test_overlap_handling.py
+++ tests/test_overlap_handling.py
@@ -1,2 +1,11 @@
-def test_fake():
-    assert 0 == 0
+from range_streams import RangeStream, example_url
+import httpx
+from ranges import Range
+
+def test_overlapping_bytes():
+    c = httpx.Client()
+    s = RangeStream(url=example_url, client=c)
+    s.handle_byte_range(Range(0,3))
+    s.handle_byte_range(Range(1,3))
+    # TODO: determine correct behaviour to assert
+    assert True == True # isinstance(s, RangeStream)
