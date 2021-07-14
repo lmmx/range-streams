3522ee44983cdb547a58a129b3c98427d88a89b4
Louis Maddox <lmmx@users.noreply.github.com>
2021-07-14 23:49:23 +0100

    typo

--- src/range_streams/__init__.py
+++ src/range_streams/__init__.py
@@ -3,7 +3,7 @@ r"""
 an API familiar to users of the standard library
 :mod:`io` module.  It uses :class:`ranges.Range`, :class:`ranges.RangeSet`,
 and :class:`ranges.RangeDict` classes (from the externally maintained
-:mod:`python-ranges: library)
+:mod:`python-ranges` library)
 to represent and look up range operations in an efficient linked
 list data structure.
 
